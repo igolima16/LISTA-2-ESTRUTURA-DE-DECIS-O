2f73fe5decb0881ac5ee8ef6fdcf8578d2f90bba
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-08-12 16:31:57 -0300

    Add files via upload

---
+++ OPDL-ex01/OPDL-ex01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPDL_ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor1, valor2;
+
+            Console.WriteLine("digite abaixo os valores que deseja comparar.");
+
+            Console.WriteLine("Digite o primeiro valor");
+            valor1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor");
+            valor2 = double.Parse(Console.ReadLine());
+
+            if (valor1 > valor2)
+            {
+                Console.WriteLine("O primeiro valor é maior!");
+
+            }
+            else
+            {
+                Console.WriteLine("O segundo valor é maior");
+
+            }
+           
+
+            Console.ReadKey();
+        }
+    }
+}
