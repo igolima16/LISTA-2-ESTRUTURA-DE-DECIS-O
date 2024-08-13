004a4e926b591b7a538a3e76265c372ce771e7fd
igolima16 <167836742+igolima16@users.noreply.github.com>
2024-08-13 19:20:47 -0300

    Add files via upload

---
+++ OPLA-ex04/OPLA-ex04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPLA_ex04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseret, alturaret, arearet;
+
+            Console.WriteLine("Digite a base do retangulo: ");
+            baseret = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura do retangulo: ");
+            alturaret = double.Parse(Console.ReadLine());
+
+            arearet = baseret*alturaret;
+
+            if (arearet > 100)
+            {
+                Console.WriteLine("O terreno é grande");
+
+            }
+
+            else
+            {
+                Console.WriteLine("O terreno é pequeno");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
